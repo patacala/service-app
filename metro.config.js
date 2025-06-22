84282918e2e33890cab660e50b88ef3396dc46ac
ajcnazar <ajcnazar@gmail.com>
2025-06-22 13:01:12 -0500

    Ejecución de builder para android exitosa

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,9 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
 // Add support for .cjs files
 config.resolver.sourceExts.push('cjs');
-
-/* config.resolver.unstable_enablePackageExports = false; */
 
 module.exports = config;
