314a93634aa44c4277388ab04af01fc87828539e
ajcnazar <ajcnazar@gmail.com>
2025-08-20 15:17:41 -0500

    inicio completar registro

--- src/infrastructure/services/api/types/auth.types.ts
+++ src/infrastructure/services/api/types/auth.types.ts
@@ -1,8 +1,8 @@
 export interface RegisterPayload {
   name: string;
   city: string;
-  /* email: string;
-  password: string; */
+  email: string;
+  phone: string;
 }
 
 export interface VerifyOtpInitialPayload {
