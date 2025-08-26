c382b79c781999eeeb195fc0b4608f8a7aacf02c
ajcnazar <ajcnazar@gmail.com>
2025-08-26 17:46:32 -0500

    actualización de servicio

--- src/store/api/apiSlice.ts
+++ src/store/api/apiSlice.ts
@@ -4,6 +4,6 @@ import { axiosBaseQuery } from './axiosBaseQuery';
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: axiosBaseQuery(),
-  tagTypes: ['Auth', 'User', 'Category', 'UserCategory', 'Service'],
+  tagTypes: ['Auth', 'User', 'Category', 'UserCategory', 'Service', 'MyServices'],
   endpoints: () => ({}),
 });
