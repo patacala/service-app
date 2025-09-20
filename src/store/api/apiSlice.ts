1563bb9e53fdc9162efd795468df4158a5357ab0
Peter Barranco <peterb.alfaro@gmail.com>
2025-09-20 04:33:31 -0400

    adjust http client and add media api

--- src/store/api/apiSlice.ts
+++ src/store/api/apiSlice.ts
@@ -4,6 +4,6 @@ import { axiosBaseQuery } from './axiosBaseQuery';
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: axiosBaseQuery(),
-  tagTypes: ['Auth', 'User', 'Category', 'UserCategory', 'Service', 'MyServices', 'MyBookServices', 'Favorite'],
+  tagTypes: ['Auth', 'User', 'Category', 'UserCategory', 'Service', 'MyServices', 'MyBookServices', 'Favorite', 'Media'],
   endpoints: () => ({}),
 });
